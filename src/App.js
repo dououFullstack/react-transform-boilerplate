83741c5933cdf2eba023ebfff15ae6718be538a5
dououFullstack <13412133311@139.com>
2017-01-03 10:04:26 +0800

    ‘add’

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import  './style/main.scss';
 // import { Home } from './Home';
 import Home from './Home'
 import Navbar from './share/Navbar';
+import AppBar from 'material-ui/lib/app-bar';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
+  componentWillMount() {
+    this.setState({renderNavBar: false})
+  }
 
   render() {
-    // console.log("this.props", this.props);
+    console.log("this.state", this.state);
     return (
       <div className="app-wrap">
-        <Navbar />
+        {this.state.renderNavBar?<Navbar />:<AppBar />}
         {this.props.children}
         <div className="app-footer">app Footer</div>
       </div>
